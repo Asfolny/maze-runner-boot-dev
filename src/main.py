e22cb91a144285e542b88e9ee0158044017a6778
Rasmus Frederiksen <rasmus@ufafarion.com>
2024-06-23 21:29:02 +0200

    Main to run maze runner and algo for solving maze

--- src/main.py
+++ src/main.py
@@ -1,4 +1,14 @@
 from window import Window
+from line import Line
+from point import Point
+from cell import Cell
+from maze import Maze
 
 win = Window(800, 600)
+
+maze = Maze(Point(5, 5), 3, 3, Point(100, 100), win)
+maze.solve()
+
 win.wait_for_close()
+
+
--- src/maze.py
+++ src/maze.py
@@ -120,3 +120,68 @@ class Maze:
 
         self._master.redraw()
         time.sleep(.2)
+
+    def solve(self):
+        return self._solve_r(0, 0)
+
+    def _solve_r(self, i, j):
+        self._animate()
+
+        # vist the current cell
+        self._cells[i][j].visited = True
+
+        # if we are at the end cell, we are done!
+        if i == self._num_cols - 1 and j == self._num_rows - 1:
+            return True
+
+        # No east wall and non-visited cell? Move
+        if (
+            i > 0
+            and not self._cells[i][j].walls["W"]
+            and not self._cells[i - 1][j].visited
+        ):
+            self._cells[i][j].draw_move(self._cells[i - 1][j])
+            if self._solve_r(i - 1, j):
+                return True
+            else:
+                self._cells[i][j].draw_move(self._cells[i - 1][j], True)
+
+        # No east wall and non-visited cell? Move
+        if (
+            i < self._num_cols - 1
+            and not self._cells[i][j].walls["E"]
+            and not self._cells[i + 1][j].visited
+        ):
+            self._cells[i][j].draw_move(self._cells[i + 1][j])
+            if self._solve_r(i + 1, j):
+                return True
+            else:
+                self._cells[i][j].draw_move(self._cells[i + 1][j], True)
+
+        # No north wall and non-visited cell? Move
+        if (
+            j > 0
+            and not self._cells[i][j].walls["N"]
+            and not self._cells[i][j - 1].visited
+        ):
+            self._cells[i][j].draw_move(self._cells[i][j - 1])
+            if self._solve_r(i, j - 1):
+                return True
+            else:
+                self._cells[i][j].draw_move(self._cells[i][j - 1], True)
+
+        # No south wall and non-visited cell? Move
+        if (
+            j < self._num_rows - 1
+            and not self._cells[i][j].walls["S"]
+            and not self._cells[i][j + 1].visited
+        ):
+            self._cells[i][j].draw_move(self._cells[i][j + 1])
+            if self._solve_r(i, j + 1):
+                return True
+            else:
+                self._cells[i][j].draw_move(self._cells[i][j + 1], True)
+
+        # Wrong way
+        return False
+
